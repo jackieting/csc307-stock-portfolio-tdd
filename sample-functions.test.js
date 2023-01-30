5509a52cfb0d76ba65313af03cb8739013372f54
jackieting <81582343+jackieting@users.noreply.github.com>
2023-01-29 17:18:58 -0800

    Delete sample-functions.test.js

--- sample-functions.test.js
+++
@@ -1,7 +0,0 @@
-const myFunctions = require("./sample-functions.js");
-
-test("Testing sum -- success", () => {
-  const target = 30;
-  const result = myFunctions.sum(12, 18);
-  expect(target).toBe(result);
-});
